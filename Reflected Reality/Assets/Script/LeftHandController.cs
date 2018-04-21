2796156d4bab99e7f4b314d13b59622de8b96118
TINAYAO <Juvia333@hotmail.com>
2018-04-22 00:29:26 +1000

    Simple drag and drop plus throwing objects. For some reason the trigger enter and exit is not working sometimes.

---
+++ Reflected Reality/Assets/Script/LeftHandController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftHandController : MonoBehaviour {
+
+	public DragAndDrop drop;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			Vector3 position = this.transform.position;
+			position.x-=0.03f;
+			this.transform.position = position;
+		}
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			Vector3 position = this.transform.position;
+			position.x+=0.03f;
+			this.transform.position = position;
+		}
+		if (Input.GetKey(KeyCode.UpArrow))
+		{
+			Vector3 position = this.transform.position;
+			position.y+=0.03f;
+			this.transform.position = position;
+		}
+		if (Input.GetKey(KeyCode.DownArrow))
+		{
+			Vector3 position = this.transform.position;
+			position.y-=0.03f;
+			this.transform.position = position;
+		}
+
+		if (Input.GetKey (KeyCode.A)) {
+			drop.DragOrPickUp ();
+		} else {
+			if (drop.getDrag()) {
+				drop.DropItem();
+			}
+		}
+	}
+
+
+}
---
+++ Reflected Reality/Assets/Script/RightHandController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightHandController : MonoBehaviour {
+
+	private Vector3 mousePos;
+	public DragAndDrop drop;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+
+
+	// Update is called once per frame
+	void Update () {
+		mousePos = Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y,1f));
+		transform.position = mousePos;
+
+		if (HasInput) {
+			drop.DragOrPickUp ();
+		} else {
+			if (drop.getDrag()) {
+				drop.DropItem();
+			}
+		}
+	}
+
+	private bool HasInput
+	{
+		get
+		{
+			// returns true if either the mouse button is down or at least one touch is felt on the screen
+			return Input.GetMouseButton(0);
+		}
+	}
+		
+}
